7f128ea876a6eefc3f496ead4fe89dac25540e7f
doloadama <doloadama@outlook.com>
2022-10-27 10:02:04 +0000

    .

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,34 +1,42 @@
-#!/usr/bin/python3
-"""web server distribution"""
-from fabric.api import *
-from fabric.state import commands, connections
-import os.path
+# deploy static
+$whisper_dirs = [ '/data/', '/data/web_static/',
+                        '/data/web_static/releases/', '/data/web_static/shared/',
+                        '/data/web_static/releases/test/'
+                  ]
 
-env.user = 'ubuntu'
-env.hosts = ["104.196.155.240", "34.74.146.120"]
-env.key_filename = "~/id_rsa"
+package {'nginx':
+  ensure  => installed,
+}
 
+file { $whisper_dirs:
+        ensure  => 'directory',
+        owner   => 'ubuntu',
+        group   => 'ubuntu',
+        recurse => 'remote',
+        mode    => '0777',
+}
+file { '/data/web_static/current':
+  ensure => link,
+  target => '/data/web_static/releases/test/',
+}
+file {'/data/web_static/releases/test/index.html':
+  ensure  => present,
+  content => 'Holberton School for the win!',
+}
 
-def do_clean(number=0):
-    """deletes out-of-date archives"""
-    local('ls -t ~/AirBnB_Clone_V2/versions/').split()
-    with cd("/data/web_static/releases"):
-        target_R = sudo("ls -t .").split()
-    paths = "/data/web_static/releases"
-    number = int(number)
-    if number == 0:
-        num = 1
-    else:
-        num = number
-    if len(target_R) > 0:
-        if len(target) == number or len(target) == 0:
-            pass
-        else:
-            cl = target[num:]
-            for i in range(len(cl)):
-                local('rm -f ~/AirBnB_Clone_V2/versions/{}'.format(target[-1]))
-        rem = target_R[num:]
-        for j in range(len(rem)):
-            sudo('rm -rf {}/{}'.format(paths, rem[-1].strip(".tgz")))
-    else:
-        pass
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+
+file_line {'deploy static':
+  path  => '/etc/nginx/sites-available/default',
+  after => 'server_name _;',
+  line  => "\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}",
+}
+
+service {'nginx':
+  ensure  => running,
+}
+
+exec {'/etc/init.d/nginx restart':
+}
