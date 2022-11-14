bc961f9d845175d18fef48aaec5ea5f484996164
doloadama <doloadama@outlook.com>
2022-11-14 19:05:32 +0000

    .

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-'''A simple Flask web application.
-'''
-from flask import Flask, render_template
-
+"""
+Starting my first Flask web application
+"""
 from models import storage
 from models.state import State
-
-
+from os import environ
+from flask import Flask, render_template
 app = Flask(__name__)
-'''The Flask application instance.'''
-app.url_map.strict_slashes = False
-
-
-@app.route('/states_list')
-def states_list():
-    '''The states_list page.'''
-    all_states = list(storage.all(State).values())
-    all_states.sort(key=lambda x: x.name)
-    ctxt = {
-        'states': all_states
-    }
-    return render_template('7-states_list.html', **ctxt)
 
 
 @app.teardown_appcontext
-def flask_teardown(exc):
-    '''The Flask app/request context end event listener.'''
+def close_db(error):
+    """
+    Remove the SQLAlchemy session
+    """
     storage.close()
 
 
-if __name__ == '__main__':
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """
+    A route that displays cities in our database in html
+    """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000, debug=True)
